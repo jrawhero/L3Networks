e848c990c5306a91a9629f60b367ad8b0f8cf566
thegrid22593 <garrett.sanderson@gmail.com>
2016-02-24 22:08:11 -0800

    sub navigation toggle

--- js/app.js
+++ js/app.js
@@ -1,23 +1,36 @@
 // New Js File
-
-
 (function() {
 
-  var interactive = {
-    init: function() {
-      this.cacheDom();
-      this.bindEvents();
-    },
+  $(document).ready(function(){
+    var interactive = {
+      init: function() {
+        this.cacheDom();
+        this.bindEvents();
+      },
 
-    cacheDom: function() {
+      cacheDom: function() {
+        this.$mainNavItem = $('.main-nav ul li a');
+        this.$underNavContainer = $('.under-nav-container');
+      },
 
-    },
+      bindEvents: function() {
+        this.$mainNavItem.on('click', this.subNavShow);
+        console.log('binding');
+      },
 
-    bindEvents: function() {
-    }
-  };
+      // Show sub nav
+      subNavShow: function(e) {
+        e.stopPropagation();
+        var $subNavToShow = $(this).attr('rel');
+        console.log($subNavToShow);
 
-  interactive.init();
+        // this.$underNavContainer.find('.under-nav-open').removeClass('under-nav-open');
 
+        $('#'+$subNavToShow).toggleClass('under-nav-open');
+      }
+    };
+
+    interactive.init();
+  });
 
 })();
