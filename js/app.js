023e7613932187408b161d1901736f426040d66e
thegrid22593 <garrett.sanderson@gmail.com>
2016-02-24 21:19:57 -0800

    working on header navigation

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// New Js File
+
+
+(function() {
+
+  var interactive = {
+    init: function() {
+      this.cacheDom();
+      this.bindEvents();
+    },
+
+    cacheDom: function() {
+
+    },
+
+    bindEvents: function() {
+    }
+  };
+
+  interactive.init();
+
+
+})();
