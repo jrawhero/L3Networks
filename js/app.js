ea28badd403f96e21273cca7b1fa73b9d63a7c14
thegrid22593 <garrett.sanderson@gmail.com>
2016-02-26 13:33:48 -0800

    Added hide sub nav on click off

--- js/app.js
+++ js/app.js
@@ -11,10 +11,13 @@
       cacheDom: function() {
         this.$mainNavItem = $('.main-nav ul li a');
         this.$underNavContainer = $('.under-nav-container');
+        this.$window = $(window);
+        this.$document = $(document);
       },
 
       bindEvents: function() {
         this.$mainNavItem.on('click', this.subNavShow);
+        this.$document.on('click', this.hideSubNav);
         console.log('binding');
       },
 
@@ -27,6 +30,12 @@
         $('.under-nav-container').find('.under-nav-open').removeClass('under-nav-open');
 
         $('#'+$subNavToShow).toggleClass('under-nav-open');
+      },
+
+      // Hide sub nav when you click off
+      hideSubNav: function(e) {
+        e.stopPropagation();
+        $('.under-nav').removeClass('under-nav-open');
       }
     };
 
