ba85f6eacd6ee344805e90b1265562e3dac90f0c
thegrid22593 <garrett.sanderson@gmail.com>
2016-03-02 22:53:58 -0800

    mouse enter on sub nav show / new smaller scroll nav

--- js/app.js
+++ js/app.js
@@ -20,9 +20,9 @@
 
       //Bind Events
       bindEvents: function() {
-        this.$mainNavItem.on('click', this.subNavShow);
+        this.$mainNavItem.on('mouseenter', this.subNavShow);
         this.$document.on('click', this.hideSubNav);
-        this.$window.scroll(this.navShrinkOnScroll);
+        this.$window.scroll(this.showScrollNav);
         this.$window.scroll(this.fixedSubNav);
 
         console.log('binding');
@@ -42,37 +42,26 @@
       // Hide sub nav when you click off
       hideSubNav: function(e) {
         e.stopPropagation();
-        $('.under-nav').removeClass('under-nav-open');
+        if($('.under-nav-open').hasClass('scrolled')) {
+          return
+        } else {
+          $('.under-nav').removeClass('under-nav-open');
+        }
       },
 
       // Fixed and Skrink Nav
-      navShrinkOnScroll: function() {
-
-        if($(window).scrollTop() > 2) {
-          $('.main-header-container').addClass('scrolled');
+      showScrollNav: function() {
+        if($(window).scrollTop() > 30) {
+          $('.main-header-container-scroll').addClass('scrolled');
         } else {
-          $('.main-header-container').removeClass('scrolled');
+          $('.main-header-container-scroll').removeClass('scrolled');
         }
       },
 
       fixedSubNav: function() {
         var headerHeight = $('.main-header-container').height();
         var currentURL = $(location).attr('pathname');
-        console.log('yes');
-
-        if($(window).scrollTop() > headerHeight && $('.under-nav').hasClass('under-nav-open')) {
-          $('.under-nav-open').addClass('scrolled');
-        } else {
-          $('.under-nav-open').removeClass('scrolled');
-        }
-
-        // if(window.location.pathname = 'Users/Garrett/Desktop/Sites/L3Networks/it-management.html') {
-        //   $('#it-management').addClass('fixed');
-        // } else if (location.href = 'Users/Garrett/Desktop/Sites/L3Networks/cloud-solutions.html') {
-        //   $('#cloud-solutions').addClass('fixed');
-        // } else {
-        //   return;
-        // }
+        console.log(currentURL);
       }
 
 
